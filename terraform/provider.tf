5f07e52ce8f8f8cbbe83000c7063f0e71ed5349d
Samad Riaz <samadriaz9@gmail.com>
2026-02-08 22:17:09 +0500

    bucket name updated

---
+++ terraform/provider.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+
+}
+
+terraform {
+    backend "s3" {
+        bucket = "teraform-github-action-v1"
+        region = "eu-north-1"
+        key = "github-action/terraform.tfstate"
+        encrypt = true
+        dynamodb_table = "teraform-github-action-lock"
+    }
+}
